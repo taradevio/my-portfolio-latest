7f9f67e57123f5fb5df838f95bc1bbbbdcb660e8
Tara <nandayavanets@mail.ru>
2025-10-18 11:58:29 +0700

    Implement code changes to enhance functionality and improve performance

--- tina/config.js
+++ tina/config.js
@@ -22,8 +22,8 @@ export default defineConfig({
   },
   media: {
     tina: {
-      publicFolder: "",
-      mediaRoot: "uploads",
+      publicFolder: "public",
+      mediaRoot: "",
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/r/content-modelling-collections/
